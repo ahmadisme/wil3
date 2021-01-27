a758a3d51896b466e6430baed17c942b3e89c830
Ahmad Imaduddin <ahmadneverlast@gmail.com>
2021-01-27 16:46:02 +0700

    update 1

---
+++ application/views/template/footer.php
@@ -0,0 +1,48 @@
+<!-- Footer -->
+<footer class="footer pt-0">
+        <div class="row align-items-center justify-content-lg-between">
+          <div class="col-lg-6">
+            <div class="copyright text-center  text-lg-left  text-muted">
+              &copy; 2020 <a href="https://www.creative-tim.com/" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+              <li class="nav-item">
+                <a href="https://www.creative-tim.com/" class="nav-link" target="_blank">Creative Tim</a>
+              </li>
+              <li class="nav-item">
+                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a href="http://blog.creative-tim.com/" class="nav-link" target="_blank">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a href="https://www.creative-tim.com/license" class="nav-link" target="_blank">License</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+  <!-- Argon Scripts -->
+  <!-- Core -->
+  <script src="<?= base_url('assets/argon/')?>vendor/jquery/dist/jquery.min.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/js-cookie/js.cookie.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+  <!-- Optional JS -->
+  <script src="<?= base_url('assets/argon/')?>vendor/datatables.net/js/jquery.dataTables.min.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/datatables.net-buttons/js/buttons.html5.min.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/datatables.net-buttons/js/buttons.flash.min.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/datatables.net-buttons/js/buttons.print.min.js"></script>
+  <script src="<?= base_url('assets/argon/')?>vendor/datatables.net-select/js/dataTables.select.min.js"></script>
+  <!-- Argon JS -->
+  <script src="<?= base_url('assets/argon/')?>js/argon.min5438.js?v=1.2.0"></script>
+</body>
+</html>
